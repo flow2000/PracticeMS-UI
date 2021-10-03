b4d56a90112c48c1dbe415e8fd316b6bcfe581c0
Cai <2380024671@QQ.com>
2021-10-03 20:21:14 +0800

    C First UI

---
+++ src/api/practice-score/practiceScore.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询实习成绩列表
+export function listPracticeScore(query) {
+  return request({
+    url: '/practice-score/practiceScore/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询实习成绩详细
+export function getPracticeScore(scoreId) {
+  return request({
+    url: '/practice-score/practiceScore/' + scoreId,
+    method: 'get'
+  })
+}
+
+// 新增实习成绩
+export function addPracticeScore(data) {
+  return request({
+    url: '/practice-score/practiceScore',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改实习成绩
+export function updatePracticeScore(data) {
+  return request({
+    url: '/practice-score/practiceScore',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除实习成绩
+export function delPracticeScore(scoreId) {
+  return request({
+    url: '/practice-score/practiceScore/' + scoreId,
+    method: 'delete'
+  })
+}
+
+// 导出实习成绩
+export function exportPracticeScore(query) {
+  return request({
+    url: '/practice-score/practiceScore/export',
+    method: 'get',
+    params: query
+  })
+}
