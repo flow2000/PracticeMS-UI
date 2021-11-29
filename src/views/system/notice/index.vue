ba260e15d80d6cd97cb0d1d6b7bc87146cd6e467
hw57584513 <1449430837@qq.com>
2021-11-29 10:53:16 +0800

    commit

--- src/views/system/notice/index.vue
+++ src/views/system/notice/index.vue
@@ -81,7 +81,7 @@
       />
 
 
-      <el-table-column label="操作人员用户名" align="center" prop="createBy" width="150" />
+      <el-table-column label="创建人用户名" align="center" prop="createBy" width="150" />
       <el-table-column label="创建人姓名" align="center" prop="nickname" width="150" />
       <el-table-column label="创建时间" align="center" prop="createTime" width="120">
         <template slot-scope="scope">
@@ -189,172 +189,172 @@
 </template>
 
 <script>
-import { listNotice, getNotice, delNotice, addNotice, updateNotice } from "@/api/system/notice";
+  import { listNotice, getNotice, delNotice, addNotice, updateNotice } from "@/api/system/notice";
 
-export default {
-  name: "Notice",
-  data() {
-    return {
-      // 遮罩层
-      loading: true,
-      // 选中数组
-      ids: [],
-      // 非单个禁用
-      single: true,
-      // 非多个禁用
-      multiple: true,
-      // 显示搜索条件
-      showSearch: true,
-      // 总条数
-      total: 0,
-      // 公告表格数据
-      noticeList: [],
-      // 弹出层标题
-      title: "",
-      // 对应某个通知的状态
-      noticeStatus: '待审核',
-      //通知类型
-      noticeType:"",
-      // 是否显示弹出层
-      open: false,
-      // 类型数据字典
-      statusOptions: [],
-      // 状态数据字典
-      typeOptions: [],
-      // 查询参数
-      queryParams: {
-        pageNum: 1,
-        pageSize: 10,
-        noticeTitle: undefined,
-        noticeType:undefined,
-        createBy: undefined,
-        status: undefined
-      },
-      // 表单参数
-      form: {},
-      // 表单校验
-      rules: {
-        noticeTitle: [
-          { required: true, message: "公告标题不能为空", trigger: "blur" }
-        ],
-        noticeType: [
-          { required: true, message: "公告类型不能为空", trigger: "change" }
-        ]
-      }
-    };
-  },
-  created() {
-    this.getList();
-    this.getDicts("sys_notice_status").then(response => {
-      this.statusOptions = response.data;
-    });
-    this.getDicts("sys_notice_type").then(response => {
-      this.typeOptions = response.data;
-    });
-  },
-  methods: {
-    /** 查询公告列表 */
-    getList() {
-      this.loading = true;
-      listNotice(this.queryParams).then(response => {
-        this.noticeList = response.rows;
-        this.total = response.total;
-        this.loading = false;
-
-      });
-    },
-    // 公告状态字典翻译
-    statusFormat(row, column) {
-      return this.selectDictLabel(this.statusOptions, row.status);
-    },
-    // 公告状态字典翻译
-    typeFormat(row, column) {
-      return this.selectDictLabel(this.typeOptions, row.noticeType);
-    },
-    // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
-    },
-    // 表单重置
-    reset() {
-      this.form = {
-        noticeId: undefined,
-        noticeTitle: undefined,
-        noticeType: undefined,
-        noticeContent: undefined,
-        status: "0"
-      };
-      this.resetForm("form");
-    },
-    /** 搜索按钮操作 */
-    handleQuery() {
-      this.queryParams.pageNum = 1;
-      this.getList();
-    },
-    /** 重置按钮操作 */
-    resetQuery() {
-      this.resetForm("queryForm");
-      this.handleQuery();
-    },
-    // 多选框选中数据
-    handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.noticeId)
-      this.single = selection.length!=1
-      this.multiple = !selection.length
-    },
-    /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加公告";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const noticeId = row.noticeId || this.ids
-      getNotice(noticeId).then(response => {
-        this.form = response.data;
-        if(this.form.status=='0')
-          this.noticeStatus ='待审核';
-        else if(this.form.status=='1')
-          this.noticeStatus ='审核通过'
-        else this.noticeStatus ='审核不通过'
-        this.open = true;
-        this.title = "修改公告";
-      });
-    },
-    /** 提交按钮 */
-    submitForm: function() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.noticeId != undefined) {
-            if(this.noticeStatus=='待审核')
-              this.form.status='0'
-            if(this.noticeStatus=='审核通过')
-              this.form.status='1'
-            if(this.noticeStatus=='审核不通过')
-              this.form.status='-1'
-            console.log(this.form);
-            updateNotice(this.form).then(response => {
-              this.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
-          } else {
-            addNotice(this.form).then(response => {
-              this.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
-          }
+  export default {
+    name: "Notice",
+    data() {
+      return {
+        // 遮罩层
+        loading: true,
+        // 选中数组
+        ids: [],
+        // 非单个禁用
+        single: true,
+        // 非多个禁用
+        multiple: true,
+        // 显示搜索条件
+        showSearch: true,
+        // 总条数
+        total: 0,
+        // 公告表格数据
+        noticeList: [],
+        // 弹出层标题
+        title: "",
+        // 对应某个通知的状态
+        noticeStatus: '待审核',
+        //通知类型
+        noticeType:"",
+        // 是否显示弹出层
+        open: false,
+        // 类型数据字典
+        statusOptions: [],
+        // 状态数据字典
+        typeOptions: [],
+        // 查询参数
+        queryParams: {
+          pageNum: 1,
+          pageSize: 10,
+          noticeTitle: undefined,
+          noticeType:undefined,
+          createBy: undefined,
+          status: undefined
+        },
+        // 表单参数
+        form: {},
+        // 表单校验
+        rules: {
+          noticeTitle: [
+            { required: true, message: "公告标题不能为空", trigger: "blur" }
+          ],
+          noticeType: [
+            { required: true, message: "公告类型不能为空", trigger: "change" }
+          ]
         }
+      };
+    },
+    created() {
+      this.getList();
+      this.getDicts("sys_notice_status").then(response => {
+        this.statusOptions = response.data;
+      });
+      this.getDicts("sys_notice_type").then(response => {
+        this.typeOptions = response.data;
       });
     },
-    /** 删除按钮操作 */
-    handleDelete(row) {
-      const noticeIds = row.noticeId || this.ids
-      this.$confirm('是否确认删除公告编号为"' + noticeIds + '"的数据项?', "警告", {
+    methods: {
+      /** 查询公告列表 */
+      getList() {
+        this.loading = true;
+        listNotice(this.queryParams).then(response => {
+          this.noticeList = response.rows;
+          this.total = response.total;
+          this.loading = false;
+
+        });
+      },
+      // 公告状态字典翻译
+      statusFormat(row, column) {
+        return this.selectDictLabel(this.statusOptions, row.status);
+      },
+      // 公告状态字典翻译
+      typeFormat(row, column) {
+        return this.selectDictLabel(this.typeOptions, row.noticeType);
+      },
+      // 取消按钮
+      cancel() {
+        this.open = false;
+        this.reset();
+      },
+      // 表单重置
+      reset() {
+        this.form = {
+          noticeId: undefined,
+          noticeTitle: undefined,
+          noticeType: undefined,
+          noticeContent: undefined,
+          status: "0"
+        };
+        this.resetForm("form");
+      },
+      /** 搜索按钮操作 */
+      handleQuery() {
+        this.queryParams.pageNum = 1;
+        this.getList();
+      },
+      /** 重置按钮操作 */
+      resetQuery() {
+        this.resetForm("queryForm");
+        this.handleQuery();
+      },
+      // 多选框选中数据
+      handleSelectionChange(selection) {
+        this.ids = selection.map(item => item.noticeId)
+        this.single = selection.length!=1
+        this.multiple = !selection.length
+      },
+      /** 新增按钮操作 */
+      handleAdd() {
+        this.reset();
+        this.open = true;
+        this.title = "添加公告";
+      },
+      /** 修改按钮操作 */
+      handleUpdate(row) {
+        this.reset();
+        const noticeId = row.noticeId || this.ids
+        getNotice(noticeId).then(response => {
+          this.form = response.data;
+          if(this.form.status=='0')
+            this.noticeStatus ='待审核';
+          else if(this.form.status=='1')
+            this.noticeStatus ='审核通过'
+          else this.noticeStatus ='审核不通过'
+          this.open = true;
+          this.title = "修改公告";
+        });
+      },
+      /** 提交按钮 */
+      submitForm: function() {
+        this.$refs["form"].validate(valid => {
+          if (valid) {
+            if (this.form.noticeId != undefined) {
+              if(this.noticeStatus=='待审核')
+                this.form.status='0'
+              if(this.noticeStatus=='审核通过')
+                this.form.status='1'
+              if(this.noticeStatus=='审核不通过')
+                this.form.status='-1'
+              console.log(this.form);
+              updateNotice(this.form).then(response => {
+                this.msgSuccess("修改成功");
+                this.open = false;
+                this.getList();
+              });
+            } else {
+              addNotice(this.form).then(response => {
+                this.msgSuccess("新增成功");
+                this.open = false;
+                this.getList();
+              });
+            }
+          }
+        });
+      },
+      /** 删除按钮操作 */
+      handleDelete(row) {
+        const noticeIds = row.noticeId || this.ids
+        this.$confirm('是否确认删除公告编号为"' + noticeIds + '"的数据项?', "警告", {
           confirmButtonText: "确定",
           cancelButtonText: "取消",
           type: "warning"
@@ -364,7 +364,7 @@ export default {
           this.getList();
           this.msgSuccess("删除成功");
         }).catch(() => {});
+      }
     }
-  }
-};
+  };
 </script>
