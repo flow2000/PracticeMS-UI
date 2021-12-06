4f73039b2e0f9d1b0fd80e5abb7e3f382ce8503b
梁情晓 <1257322785@qq.com>
2021-12-06 10:48:55 +0800

    Merge branch 'master' of E:\codeManage\实习管理系统\practice-ms-ui with conflicts.

--- src/views/index_stu.vue
+++ src/views/index_stu.vue
@@ -1,144 +1,92 @@
 <template>
-  <div class="app-container home" >
+  <div id="magicalDragScene" class="mc-root mc-ui-absolute-pane" style="height: 1000px;min-height: 1000px;min-width: 1500px" mcnetline="100">
+    <div id="main-panle" class="mc-ui-grid-pane" style="height: 1000px; width: auto;margin:1%;min-height: 1000px;min-width: 1500px">
+      <div class="mc-ui-grid-item top-panle">
+        <div style="font-size:xx-large;color: #1c84c6;width: 30%;position: relative;float: left">
+            |欢迎使用本系统！<br />
+            <div style="font-size: x-large;color: #0eaa8b;">-{{roleGroup}}：{{user.nickName}}</div>
 
-      <el-row :gutter="20">
-        <el-col :sm="24" :lg="24">
-          <div class="mc-ui-grid-item top-panle">
-            <div style="font-size:xx-large;color: #1c84c6;width: 30%;float: left">
-                <div style="padding-top: 30px;font-size: x-large;color: #0eaa8b;">{{this.timeState}},{{roleGroup}} {{user.nickName}}</div>
-            </div>
-            <div>
-              <el-image :src="qrCode" style="width: 100px; float: right"></el-image>
-            </div>
-            <!--公告详细信息-->
-            <el-dialog
-              title="系统通知"
-              :visible.sync="noticeDetail.visible"
-              width="35%"
-            >
-              <p style="text-align: center;font-size: 20px;margin-top: 0">{{noticeDetail.title}}</p>
-              <div style="font-size: 15px;color: #717171;
-              width: 50%;float: left;
-              text-align: left;padding-left: 5%">
-                发布者：{{noticeDetail.publisher}}
-              </div>
-              <div style="font-size: 15px;color: #717171;
-              width: 50%;float: right;
-              text-align: right;;padding-right: 5%">
-                发布时间：{{noticeDetail.updateTime}}
-              </div>
-              <br />
-              <div style="font-size: 16px;padding: 5% 10%" v-html="noticeDetail.content"></div>
-            </el-dialog>
-            <!--所有公告一览-->
-            <el-drawer
-              title="系统通知"
-              :visible.sync="allNotice.visible"
-            >
-              <el-collapse class="allNotice" accordion>
-                <el-collapse-item v-for="item in noticeList" :title="'['+[(item.updateTime.split(' '))[0]]+'] '+item.noticeTitle">
-
-                  <div style="font-size: 12px;color: #717171;
-              width: 50%;float: left;
-              text-align: left;">
-                    发布者：{{item.nickname}}
-                  </div>
-                  <div style="font-size: 12px;color: #717171;
-              width: 50%;float: right;
-              text-align: right;">
-                    发布时间：{{item.updateTime}}
-                  </div>
-                  <br/>
-                  <div style="font-size: 12px;padding: 0% 2%" v-html="item.noticeContent"></div>
-                </el-collapse-item>
-              </el-collapse>
-            </el-drawer>
+        </div>
+        <div>
+          <el-image :src="qrCode" style="width: 100px;position: relative;"></el-image>
+        </div>
+        <!--公告详细信息-->
+        <el-dialog
+          title="系统通知"
+          :visible.sync="noticeDetail.visible"
+          width="35%"
+        >
+          <p style="text-align: center;font-size: 20px;margin-top: 0">{{noticeDetail.title}}</p>
+          <div style="font-size: 15px;color: #717171;
+          width: 50%;float: left;
+          text-align: left;padding-left: 5%">
+            发布者：{{noticeDetail.publisher}}
           </div>
-        </el-col>
-      </el-row>
+          <div style="font-size: 15px;color: #717171;
+          width: 50%;float: right;
+          text-align: right;;padding-right: 5%">
+            发布时间：{{noticeDetail.updateTime}}
+          </div>
+          <br />
+          <div style="font-size: 16px;padding: 5% 10%" v-html="noticeDetail.content"></div>
+        </el-dialog>
+        <!--所有公告一览-->
+        <el-drawer
+          title="系统通知"
+          :visible.sync="allNotice.visible"
+        >
+          <el-collapse class="allNotice" accordion>
+            <el-collapse-item v-for="item in noticeList" :title="'['+[(item.updateTime.split(' '))[0]]+'] '+item.noticeTitle">
 
-
-    <el-row :gutter="20">
-      <el-col :sm="24" :lg="16">
-        <div class="mc-ui-grid-item left-mid">
-          <span>考勤记录</span>
-          <hr/>
-          <el-calendar v-model="mouthNow">
-            <template
-              slot="dateCell"
-              slot-scope="{date, data}">
-              {{ data.day.split('-').slice(2).join('-') }}
-              <div v-for="item in punchSucceedDate">
-                <div v-if="item.indexOf(data.day)!==-1">
-                  <p style="text-align: center"><i class="el-icon-success" style="color: #0ab685"/>签到成功</p>
-                </div>
+              <div style="font-size: 12px;color: #717171;
+          width: 50%;float: left;
+          text-align: left;">
+                发布者：{{item.nickname}}
               </div>
-              <div v-for="item in punchFailedDate">
-                <div v-if="item.indexOf(data.day)!==-1">
-                  <p style="text-align: center"><i class="el-icon-error" style="color: #dc1a2e"/>签到失败</p>
-                </div>
+              <div style="font-size: 12px;color: #717171;
+          width: 50%;float: right;
+          text-align: right;">
+                发布时间：{{item.updateTime}}
               </div>
-            </template>
-          </el-calendar>
-        </div>
-      </el-col>
-      <el-col :sm="24" :lg="8">
-        <div class="mc-ui-grid-item right-mid" style=" width: auto;margin:1%;min-height: 730px">
-          <el-descriptions v-if="practiceInfo.info != null" direction="vertical" :column="2" border>
-            <el-descriptions-item label="学号">{{this.practiceInfo.user.userName}}</el-descriptions-item>
-            <el-descriptions-item label="手机号" :span="2">{{this.practiceInfo.user.phonenumber}}</el-descriptions-item>
-            <el-descriptions-item label="实习类型">集中实习</el-descriptions-item>
-            <el-descriptions-item :span="2" label="所属班级">
-              <span>{{this.practiceInfo.user.dept.deptName}}</span>
-            </el-descriptions-item>
-            <el-descriptions-item label="实习开始时间">
-              {{this.practiceInfo.info.entryTime}}
-            </el-descriptions-item>
-            <el-descriptions-item :span="2" label="实习结束时间">
-              {{this.practiceInfo.info.endingTime}}
-            </el-descriptions-item>
-            <el-descriptions-item label="指导老师">
-              <span>{{this.practiceInfo.info.teacher.nickName}}</span>
-            </el-descriptions-item>
-            <el-descriptions-item :span="2" label="实习状态">
-              <span v-if="this.practiceInfo.info.status == 0">实习中</span>
-              <span v-else>未实习</span>
-            </el-descriptions-item>
-            <el-descriptions-item :span="3" label="实习地址" style="word-break: break-all;word-wrap: break-word;" >{{this.practiceInfo.info.baseInfo.baseAddress}}</el-descriptions-item>
-          </el-descriptions>
+              <br/>
+              <div style="font-size: 12px;padding: 0% 2%" v-html="item.noticeContent"></div>
+            </el-collapse-item>
+          </el-collapse>
+        </el-drawer>
+      </div>
 
-          <el-descriptions v-if="practiceInfo.location != null" direction="vertical" :column="2" border>
-            <el-descriptions-item label="学号">{{this.practiceInfo.user.userName}}</el-descriptions-item>
-            <el-descriptions-item label="手机号" :span="2">{{this.practiceInfo.user.phonenumber}}</el-descriptions-item>
-            <el-descriptions-item label="实习类型">集中实习</el-descriptions-item>
-            <el-descriptions-item :span="2" label="所属班级">
-              <span>{{this.practiceInfo.user.dept.deptName}}</span>
-            </el-descriptions-item>
-            <el-descriptions-item label="实习开始时间">
-              {{this.practiceInfo.info.entryTime}}
-            </el-descriptions-item>
-            <el-descriptions-item :span="2" label="实习结束时间">
-              {{this.practiceInfo.info.endingTime}}
-            </el-descriptions-item>
-            <el-descriptions-item label="指导老师">
-              <span>{{this.practiceInfo.info.teacher.nickName}}</span>
-            </el-descriptions-item>
-            <el-descriptions-item :span="2" label="实习状态">
-              <span v-if="this.practiceInfo.info.status == 0">实习中</span>
-              <span v-else>未实习</span>
-            </el-descriptions-item>
-            <el-descriptions-item :span="3" label="实习地址" style="word-break: break-all;word-wrap: break-word;" >{{this.practiceInfo.info.baseInfo.baseAddress}}</el-descriptions-item>
-          </el-descriptions>
-        </div>
-      </el-col>
-    </el-row>
+      <div class="mc-ui-grid-item left-mid">
+        <span>考勤记录</span>
+        <hr/>
+        <el-calendar v-model="mouthNow">
+          <template
+          slot="dateCell"
+          slot-scope="{date, data}">
+            {{ data.day.split('-').slice(2).join('-') }}
+            <div v-for="item in punchSucceedDate">
+              <div v-if="item.indexOf(data.day)!==-1">
+                <p style="text-align: center"><i class="el-icon-success" style="color: #0ab685"/>签到成功</p>
+              </div>
+            </div>
+            <div v-for="item in punchFailedDate">
+              <div v-if="item.indexOf(data.day)!==-1">
+                <p style="text-align: center"><i class="el-icon-error" style="color: #dc1a2e"/>签到失败</p>
+              </div>
+            </div>
+        </template>
+        </el-calendar>
+      </div>
+
+      <div class="mc-ui-grid-item right-mid">
+
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
   import { getUserProfile } from '../api/system/user'
-  import { getStudentPracticeInfo } from "@/api/decentralize/decentralize";
   import{listPunch} from '../api/punch/punch'
   import bus from '../bus'
   import qr from '@/assets/images/qr.png'
@@ -157,8 +105,6 @@
         //通知列表
         noticeList: [],
         noticeSlims:[],
-        practiceInfo:[],
-        timeState : null,
         //通知条数
         noticetotal:0,
         //通知详细窗口
@@ -196,8 +142,6 @@
     created() {
       // 获取当前用户信息
       this.getUser();
-      this.getStudentPracticeInfo();
-      this.getTimeState();
     },
     updated() {
       // 考勤记录参数设置
@@ -217,45 +161,6 @@
 
         });
       },
-      /** 查询学生实习信息 */
-      getStudentPracticeInfo() {
-        this.loading = true;
-        getStudentPracticeInfo().then(response => {
-          this.practiceInfo = response.data;
-          console.log(this.practiceInfo)
-          if(this.practiceInfo.location != null) this.practiceInfo.status = parseInt(this.practiceInfo.status)
-          if(response.data.status == '1'){
-            this.prePracticeInfo = response.data;
-            this.practiceInfo.info = null
-            this.practiceInfo.location = null
-          }
-          if(response.data.status == '3'){
-            this.remark = "reset";
-          }
-          this.loading = false;
-        });
-      },
-      getTimeState() {
-        // 获取当前时间
-        let timeNow = new Date();
-        // 获取当前小时
-        let hours = timeNow.getHours();
-        // 设置默认文字
-        let text = ``;
-        // 判断当前时间段
-        if (hours >= 4 && hours <= 10) {
-          text = `早上好`;
-        } else if (hours > 10 && hours <= 14) {
-          text = `中午好`;
-        } else if (hours > 14 && hours <= 18) {
-          text = `下午好`;
-        } else if (hours > 18 && hours <= 24 || hours < 4) {
-          text = `晚上好`;
-        }
-        console.log(text)
-        // 返回当前时间段对应的状态
-        this.timeState = text
-      },
       /* 获取考勤记录 */
       getPunch(){
         //console.log(this.punchParams)
@@ -268,6 +173,8 @@
             else
               this.punchFailedDate.push(item.createTime);
           }
+          console.log(this.punchSucceedDate)
+          console.log(this.punchFailedDate)
           this.punchTotal=response.total;
         })
       }
@@ -290,17 +197,31 @@
 </script>
 
 <style scoped>
+  #main-panle{
+    display: grid;
+    grid-template-columns:0.3fr 0.3fr 0.4fr ;
+    grid-template-rows:0.1fr 0.6fr 0.25fr 0.05fr;
+    gap: 1% 1%;
+    grid-template-areas:
+      "top-panle top-panle top-panle"
+      "left-mid left-mid right-mid"
+      "left-mid left-mid right-mid"
+      "left-bottom left-bottom right-bottom"
+  ;
+
+  }
+
   .top-panle{
+    grid-area: top-panle;
     padding: 1% 1.5%;
-    margin-bottom: 1.5%;
     border: 1px solid #e0e0e0;
     border-radius: 10px;
     box-shadow: 3px 3px 10px 0px rgba(1,1,1,0.2);
-    height: 130px;
-    max-width: 100%;
+    max-height: 110px;
   }
 
   .left-mid {
+    grid-area: left-mid;
     padding: 2%;
     border: 1px solid #e0e0e0;
     border-radius: 10px;
@@ -308,6 +229,7 @@
   }
 
   .right-mid {
+    grid-area: right-mid;
     padding: 2%;
     border: 1px solid #e0e0e0;
     border-radius: 10px;
